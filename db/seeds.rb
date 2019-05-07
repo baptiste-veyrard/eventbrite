b270f978293be4f63d65d478d9ac2f99c1868c07
Baptiste Veyrard <baptiste.veyrard@gmail.com>
2019-05-07 15:32:31 +0200

    maj

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Attendance.destroy_all
 2.times do 
 	u = User.create!(
 		email: "#{Faker::Internet.username}@yopmail.com",
-		encrypted_password: Faker::Number.number(10),
+		password: Faker::Number.number(10),
 		first_name: Faker::Name.first_name,
 		last_name: Faker::Name.last_name,
 		description: Faker::Lorem.sentence(15)
