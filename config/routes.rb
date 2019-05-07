a2fcaf3499ab95e7ba7f1e4cab5d16f989c81fc8
Baptiste Veyrard <baptiste.veyrard@gmail.com>
2019-05-07 18:29:24 +0200

    maj

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'event/index'
   devise_for :users
   resources :events
+  resources :user, only: [:show]
 
  root to: 'event#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
